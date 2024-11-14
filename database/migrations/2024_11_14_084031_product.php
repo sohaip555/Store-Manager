5c876391ad57913dbb2628eb8f9978227df52540
Sohaip Abughrara <sohupabughrara@gmail.com>
2024-11-14 10:41:36 +0200

    updateing

--- database/migrations/2024_11_14_084031_product.php
+++ database/migrations/2024_11_14_084031_product.php
@@ -15,7 +15,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignIdFor(brand::class);
+            $table->foreignIdFor(brand::class)->constrained();
             $table->string('item_code')->nullable();
             $table->integer('price');
             $table->integer('quantity');
